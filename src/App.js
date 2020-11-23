0523d4b2813889851a1318de5b8f7a197a9a9ce9
gavriley-es <gavriley-es@mail.ru>
2020-11-23 11:52:54 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Header/Menu/Menu";
 import Main from "./components/Content/Main/Main";
+import Puzzle from "./components/Content/Puzzle/Puzzle";
+import Guess from "./components/Content/Guess/Guess";
+import GuessPvP from "./components/Content/GuessPvP/GuessPvP";
+import Password from "./components/Content/Password/Password";
+import WorkedOut from "./components/Content/WorkedOut/WorkedOut";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="wrapper">
-      <div className="content">
-        <Menu/>
-        <Main/>
+    <BrowserRouter>
+      <div className="wrapper">
+        <div className="content">
+          <Menu />
+          <Route path='/main' component={Main} />
+          <Route path='/puzzle' component={Puzzle} />
+          <Route path='/guess' component={Guess} />
+          <Route path='/guessPvP' component={GuessPvP} />
+          <Route path='/passwordGeneration' component={Password} />
+          <Route path='/timeWorkedOut' component={WorkedOut} />
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -3,35 +3,35 @@ import cls from './Menu.module.css'
 const Menu = () => {
   return (
     <nav className={cls.menu}>
-        <div className={cls.title}><a href="index.php">ОтЛичныйСайт</a></div>
+        <div className={cls.title}><a href="/main">ОтЛичныйСайт</a></div>
         <ul className={cls.main}>
-          <li><a href="index.php">Главная</a></li>
+          <li><a href="/main">Главная</a></li>
           <li>Мини игры &#9660; 
             <ul className={cls.drop}>
               <div>
-                <li><a href="puzzle.php">Загадки</a></li>
-                <li><a href="guess.php">Угадайка</a></li>
-                <li><a href="guess-2.php">Угадайка мультиплеер</a></li>
+                <li><a href="/puzzle">Загадки</a></li>
+                <li><a href="/guess">Угадайка</a></li>
+                <li><a href="/guessPvP">Угадайка мультиплеер</a></li>
               </div>
             </ul>
           </li>
           <li>Полезное &#9660;
             <ul className={cls.drop}>
               <div>
-                <li><a href="passwordGeneration.php">Генератор паролей</a></li>
-                <li><a href="time_worked_out.php">Расчет рабочего времени</a></li>
+                <li><a href="/passwordGeneration">Генератор паролей</a></li>
+                <li><a href="/timeWorkedOut">Расчет рабочего времени</a></li>
               </div>
             </ul>
           </li>
           <li>Практика &#9660; 
             <ul className={cls.drop}>
               <div>
-                <li><a href="flex.php">flex-шпаргалка</a></li>
-                <li><a href="size.php">size-практика</a></li>
+                <li><a href="/flex">flex-шпаргалка</a></li>
+                <li><a href="/size">size-практика</a></li>
               </div>
             </ul>
           </li>
-          <li><a href="bombermen.php">"Bombermen"</a></li>
+          <li><a href="/bombermen">"Bombermen"</a></li>
           <div className={cls.marker}></div>
         </ul>
       </nav>
